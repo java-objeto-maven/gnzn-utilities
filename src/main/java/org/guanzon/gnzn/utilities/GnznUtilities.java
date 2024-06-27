504be7f11f1d555c4205bc128db04749192f713a
xurpas7 <57973174+xurpas7@users.noreply.github.com>
2024-06-27 14:49:11 +0800

    Initial commit

---
+++ src/main/java/org/guanzon/gnzn/utilities/GnznUtilities.java
@@ -0,0 +1,8 @@
+package org.guanzon.gnzn.utilities;
+
+public class GnznUtilities {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/org/guanzon/gnzn/utilities/test/testSendMail.java
@@ -0,0 +1,48 @@
+package org.guanzon.gnzn.utilities.test;
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class testSendMail {
+    public static void main(String [] args){
+        final String username = "misguanzon03@gmail.com";
+        final String password = "nbss lpkq mdbq wfwq";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+            new Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(username, password);
+                }
+            });
+
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse("xurpas7@gmail.com"));
+            message.setSubject("Subject");
+            message.setText("Hello, World!");
+
+            Transport.send(message);
+
+            System.out.println("Email sent successfully!");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

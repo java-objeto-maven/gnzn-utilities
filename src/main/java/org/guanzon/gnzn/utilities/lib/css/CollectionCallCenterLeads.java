adadfc155bce164064133252aa2b54eadaa8132d
Michael Cuison <77145878+mtcuison@users.noreply.github.com>
2026-02-05 17:31:19 +0800

    20260205

---
+++ src/main/java/org/guanzon/gnzn/utilities/lib/css/CollectionCallCenterLeads.java
@@ -0,0 +1,17 @@
+package org.guanzon.gnzn.utilities.lib.css;
+
+import org.guanzon.appdriver.base.GRider;
+import org.json.simple.JSONObject;
+
+public class CollectionCallCenterLeads {
+    GRider _instance;
+    JSONObject _json;
+    
+    public CollectionCallCenterLeads(GRider foValue){
+        _instance = foValue;
+    }
+    
+    public JSONObject Create(){
+        
+    }
+}
